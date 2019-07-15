b7877972a6df1b23eb121e2f5c7086a84ea36bf7
dengmiaomiao <tracey_long@163.com>
2019-07-15 20:16:04 +0800

    "增加演出详情路由组件"

---
+++ jucoo/src/views/showinfo.js
@@ -0,0 +1,14 @@
+import React from 'react';
+class Showinfo extends React.Component{
+    constructor(){
+        super();
+    }
+    render(){
+        return (
+            <div>
+                演出详情
+            </div>
+        )
+    }
+}
+export default Showinfo;
