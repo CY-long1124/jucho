f37316471293b8452092e96a41fee5281f1b5979
langzisuo <nanian1124@163.com>
2019-07-16 21:45:17 +0800

    首页部分渲染

---
+++ jucoo/src/components/jucoo/classfy.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import {
+    withRouter
+} from 'react-router-dom'
+import axios from 'axios'
+import {
+    connect
+} from 'react-redux'
+import '../../assets/css/classfy.css'
+class Classfy extends React.Component{
+    render(){
+        return(
+            <div className="classfy">
+                {
+                    this.props.classfyList.map((v,i)=>{
+                        return (
+                            <div key={i}>
+                                <img src={v.pic} alt=""/>
+                                <span>{v.name}</span>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        )
+    }
+    componentWillMount(){
+        this.props.getClassFyList();
+    }
+    
+}
+const mapStateToProps = (state) => {
+    console.log(state.classfy.classfyList)
+    return {
+       classfyList : state.classfy.classfyList
+    }
+}
+const mapDispatchToProps = (dispatch) => {
+    return {
+        getClassFyList(){
+            dispatch((dispatch)=>{
+                axios.get("/juco/home/getClassifyItem?city_id=0&operation_limit=5&version=6.0.1&referer=2&timestamp=1563269989").then(({data})=>{
+                    // console.log(data.data.classify_list);
+                    dispatch({
+                        type:"GET_CLASSFYLIST",
+                        payload:{
+                            classfyList : data.data.classify_list,
+                        }
+                    })
+                })
+            })
+           
+        }
+    }
+}
+export default connect(mapStateToProps,mapDispatchToProps)(withRouter(Classfy));
--- jucoo/src/views/jucheng.js
+++ jucoo/src/views/jucheng.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import HomeHead from '../components/jucoo/homeHead'
+import Classfy from '../components/jucoo/classfy'
 import '../assets/css/homeHead.css'
 
 class Jucoo extends React.Component{
@@ -11,7 +12,10 @@ class Jucoo extends React.Component{
             <div className="father">
                 <HomeHead></HomeHead>
                 <div style={{width:"100%",height:"196px",background:"purple"}}></div>
-                
+                <Classfy></Classfy>
+                <div className="adver">
+                    <img src="https://image.juooo.com//group1/M00/03/7A/rAoKNV0jflaAL5_EAAD3QE0LKFo824.png" alt=""/>
+                </div>
             </div>
         )
     }
