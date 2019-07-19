d3f34927a38c7441aae4f25a45518520db50d352
prr <y_gang95@163.com>
2019-07-19 22:37:43 +0800

    巡回演出数据渲染并调节cs

--- jucoo/src/App.js
+++ jucoo/src/App.js
@@ -11,7 +11,8 @@ import {
 } from 'react-router-dom'
 
 import router from "./router"
-import BottomNav from './components/bottomNav'
+import BottomNav from './components/bottomNav';
+import HotBlock from "./components/jucoo/hotBlock";
 import Jucheng from './views/jucheng'
 import Theatre from './views/theatre'
 import Eticket from './views/eticket'
@@ -37,9 +38,7 @@ class App extends React.Component{
 					    <NavLink to={"/login" }></NavLink>
 					    <NavLink to={"/register" }></NavLink>
 							<NavLink to={"/theatreStyleCenter"}></NavLink>
-						
-						
-					
+
 							{
 								router.map((v,i)=>{
 									return(
