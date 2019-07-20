5c3abea4208114efe2b91482f1888e818ae7d2e6
jj <jing_jing67@163.com>
2019-07-20 15:38:23 +0800

    购物车

--- jucoo/api/server.js
+++ jucoo/api/server.js
@@ -4,7 +4,7 @@ const db = require("./module/db")
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 
-app.post("/login/register",function(req,res){
+app.post("/register",function(req,res){//注册
 	// console.log(req)
 	db.insertOne("registerList",{
 		tel:req.body.tel,
@@ -17,10 +17,10 @@ app.post("/login/register",function(req,res){
 		})
 	})
 })
-app.post("/login/login",function(req,res){
+app.post("/login",function(req,res){//登录
 	const tel=req.body.tel;
 	const password=req.body.password
-	console.log(tel,password)
+	// console.log(tel,password)
 	db.findOne("registerList",{
 		tel,
 		password
@@ -37,6 +37,49 @@ app.post("/login/login",function(req,res){
 				msg:"未注册",
 			})
 		}
+	})
+})
+
+app.post("/buyEticket",function(req,res){//像购物车数据库添加信息
+	db.insertOne("buyEticketList",{
+		img:req.body.show_img,
+		name:req.body.show_name,
+		show_id:req.body.show_id,
+		price:req.body.show_price,
+		addTime:Date.now()
+	},function(err,results){
+		res.json({
+			ok:1,
+			msg:"添加成功"
+		})
+	})
+})
+
+app.get("/eticket",function(req,res){//从数据库里拿东西渲染票夹列表
+	db.find("buyEticketList",{
+		sortObj:{
+			addTime:-1
+		}
+	},function(err,eticketList){
+		// console.log(eticketList)
+		res.json({
+			ok:1,
+			msg:"获取成功",
+			eticketList
+		})
+	})
+})
+
+
+app.delete("/eticket/:id",function(req,res){
+	const id=req.params.id
+	// console.log(id)
+	db.deleteOneById("buyEticketList",id,function(err,eticketList){
+		res.json({
+			ok:1,
+			msg:"删除成功",
+			eticketList
+		})
 	})
 })
 app.listen(80,function(){
--- jucoo/src/views/showinfo.js
+++ jucoo/src/views/showinfo.js
@@ -6,15 +6,27 @@ import {
 import {
     bindActionCreators
 } from "redux"
+import axios from "axios"
 import showinfoCreator from "../store/actionCreator/showinfoCreator"
 import ShowinfoHeader from "../components/showinfo/showinfo-header"
 import ShowinfoMain from "../components/showinfo/showinfo-main"
 import ShowinfoTour from "../components/showinfo/showinfo-tour"
 import ShowinfoRecommend from "../components/showinfo/shoeinfo-recommend"
 class Showinfo extends React.Component{
-    constructor(){
-        super();
+    constructor(props){
+        super(props);
     }
+	buyEticket(v){
+		// console.log(v,v.pic,v.schedular_id,v.show_name,v.price_range)
+		axios.post("/login/buyEticket",{
+			show_img:v.pic,
+			show_id:v.schedular_id,
+			show_name:v.show_name,
+			show_price:v.price_range
+		}).then(({data})=>{
+			console.log(data)
+		})
+	}
     render(){
         if(this.props.showinfoState.static_data){
             // console.log(this.props.showinfoState.static_data,"000");
@@ -34,7 +46,7 @@ class Showinfo extends React.Component{
                             <img className="bottom-bar__client-service__img .img" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACgAAAAoCAMAAAC7IEhfAAAAb1BMVEUAAABmZmZmZmZmZmZmZmZmZmZmZmZmZmZmZmZmZmZmZmZmZmZmZmZmZmZmZmZmZmZmZmZmZmZmZmZmZmZmZmZmZmZmZmZmZmZmZmZmZmZmZmZmZmZmZmZmZmZmZmZmZmZmZmZmZmZmZmZmZmZmZmbw3/OPAAAAJHRSTlMAH4DvLAySB/Tg05n5rk3opHxiwKCKOhUP4qh0M9jNbli4hSkY6Xp9AAABR0lEQVQ4y52R6ZKCMBAGB8IREg4FEVA8drff/xlXsHZLNAhl/yJDJ1/NjHxInNuzBn22eSzztL7iH+W3c15eABRVllX3r9zt1YCqAxkJagXULs8C0UNaGwH21duCforKNWyfvT3o/UtRw1MxPsFVXrjCaTqmEjbiYAPlpGBIPZfopZjHczA+OPNk8HC8QD6zBLhM74VuMZxkxYZEZkgwf303VkFaur0yBWWbsZGCkcDlBYwUt59hAklk4egSj2CjwQiHHWexeOC7RB88ibNh5wYVyntRQoWRlIMsiXIgFYiWxQhWi6ujVzezfjzDwLv3A++Gga9e4Y0mUqBLcfINqKiRO7EXhOJmB9dYltmDkRU0CeQrvJ8TbGSGPgFI+tsS6hTO4ZyYMWKsAfhqZY4KvdtpRtRFZFlUWTnGLkRXnizQdwBdL5/yC+1CKknvBpjdAAAAAElFTkSuQmCC" alt=""/>
                             <div className="bottom-bar__client-service__text">客服</div>
                         </div>
-                        <div className="bottom-bar__btn">
+                        <div className="bottom-bar__btn" onClick={this.buyEticket.bind(this,this.props.showinfoState.static_data)}>
                             选座购买
                         </div>
                     </div>
